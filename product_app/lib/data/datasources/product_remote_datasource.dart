1e12caeb7c6653a6a14d9a76d2d847fb4d6bc18e
WWexe <oeningvinicius@gmail.com>
2026-03-16 20:15:02 -0300

    adjusting sintax

--- product_app/lib/data/datasources/product_remote_datasource.dart
+++ product_app/lib/data/datasources/product_remote_datasource.dart
@@ -1,13 +1,19 @@
-class ProductRemoteDatasource {
-final HttpClient client ;
-ProductRemoteDatasource ( this . client ) ;
-Future < List < ProductModel > > getProducts () async {
-final response = await client . get (
-" https :// fakestoreapi . com / products "
-) ;
-final List data = response . data ;
-return data
-. map (( json ) = > ProductModel . fromJson ( json ) )
-. toList () ;
-}
+import 'dart:io';
+import 'dart:convert';
+import 'package:product_app/data/models/product_model.dart';
+
+class ProductRemoteDatasource{
+  final HttpClient client;
+  
+  ProductRemoteDatasource(this.client);
+  
+  Future <List<ProductModel>> getProducts()async{
+    final response = await client.get(
+      "https://fakestoreapi.com/products"
+    );
+    final List data = response.data ;
+    return data
+      .map ((json) = > ProductModel.fromJson (json))
+      .toList () ;
+  }
 }
